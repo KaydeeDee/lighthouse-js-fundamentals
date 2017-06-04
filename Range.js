19eea8ca7a199f8d9fc509d6b1f8da96aeda4005
Katie DAngelo <kd.dangelo@gmail.com>
2017-06-04 01:48:08 +0000

    Area Calc and howManyHundreds

---
+++ Range.js
@@ -0,0 +1,13 @@
+function range(start, end, step){
+
+}
+var numbers = []
+
+iceCreamFlavours.push("root beer");
+
+//Define a function range which takes 3 integer parameters: start, end, and step.
+// The function should return an array of numbers from start to end counting by step.
+// The function should return an empty array [] if given incorrect parameters, such as:
+// start, end, or step being undefined
+// end being greater than start
+// step being negative
---
+++ howManyHundreds.js
@@ -0,0 +1,9 @@
+function howManyHundreds(num){
+  return Math.floor(num / 100);
+}
+
+console.log(howManyHundreds(1000), "=?", 10);
+
+
+
+// Define a function howManyHundreds, which, given a number, returns how many hundreds fit into that number.
