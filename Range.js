36369b25e4ed12ea1b2350f190581e5d81e74041
Katie DAngelo <kd.dangelo@gmail.com>
2017-06-04 22:26:13 +0000

    More JS Lighthouse

--- Range.js
+++ Range.js
@@ -1,9 +1,20 @@
-function range(start, end, step){
+function range(start, end, step) {
+  if (step === null) step = 1;
+  var array = [];
 
+  if (step > 0) {
+    for (var i = start; i <= end; i += step)
+      array.push(i);
+  }
+  return array;
 }
-var numbers = []
 
-iceCreamFlavours.push("root beer");
+function sum(array) {
+  var total = 0;
+  for (var i = 0; i < array.length; i++)
+    total += array[i];
+  return total;
+}
 
 //Define a function range which takes 3 integer parameters: start, end, and step.
 // The function should return an array of numbers from start to end counting by step.
